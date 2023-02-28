61971e4a8cbb369518539129d38fe6147b889694
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-02-28 16:34:30 +0900

    Chore : jwt,passport 패키지 설치 #7

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from 'src/global/entities/Users';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users, Busienssusers])],
+  imports: [TypeOrmModule.forFeature([Users, Busienssusers]), JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
 })
