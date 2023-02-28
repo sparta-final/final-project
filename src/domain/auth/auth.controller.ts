13a99e1215d31ad2f7b618a720bd1cd1ea7d3d5d
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-02-28 17:09:13 +0900

    Feat : 일반유저 로그인(jwt토큰발급) #7

--- src/domain/auth/auth.controller.ts
+++ src/domain/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/loginUser.dto';
 import { PostBusinessUserDto } from './dto/postBusinessUser.dto';
 import { PostUserDto } from './dto/postUser.dto';
 
@@ -24,5 +26,19 @@ export class AuthController {
   async postBusinessUsers(@Body() postBusinessUserDto: PostBusinessUserDto) {
     const businessUser = await this.authservice.postBusinessUsers(postBusinessUserDto);
     return businessUser;
+  }
+
+  @ApiOperation({ summary: '일반유저 로그인' })
+  @ApiResponse({ status: 201, description: '회원가입 성공' })
+  @ApiResponse({ status: 400, description: '회원가입 실패' })
+  @Post('user/login')
+  async userlogin(@Body() loginUserDto: LoginUserDto, @Res() res: Response) {
+    const tokens = await this.authservice.userlogin(loginUserDto);
+    res.cookie('access_token', tokens.AccessToken, {
+      httpOnly: true,
+      // secure: true,
+      sameSite: 'none',
+    });
+    return res.status(200).json({ message: '로그인 성공' });
   }
 }
--- src/domain/auth/auth.service.ts
+++ src/domain/auth/auth.service.ts
@@ -6,12 +6,16 @@ import { Repository } from 'typeorm';
 import { PostUserDto } from './dto/postUser.dto';
 import * as bcrypt from 'bcrypt';
 import { Busienssusers } from 'src/global/entities/Busienssusers';
+import { LoginUserDto } from './dto/loginUser.dto';
+import { JwtPayload } from './types/jwtPayload.type';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(Users) private userRepo: Repository<Users>,
-    @InjectRepository(Busienssusers) private businessUserRepo: Repository<Busienssusers>
+    @InjectRepository(Busienssusers) private businessUserRepo: Repository<Busienssusers>,
+    private jwtService: JwtService
   ) {}
 
   /**
@@ -53,5 +57,49 @@ export class AuthService {
       name: postBusinessUserDto.name,
       phone: postBusinessUserDto.phone,
     });
+  }
+
+  /**
+   * 일반유저 로그인
+   * @author 김승일
+   * @param loginUserDto
+   */
+  async userlogin(loginUserDto: LoginUserDto) {
+    const existUser = await this.userRepo.findOne({ where: { email: loginUserDto.email } });
+    if (!existUser) throw new ConflictException('이메일이 존재하지 않습니다.');
+    const isMatch = await bcrypt.compare(loginUserDto.password, existUser.password);
+    if (!isMatch) throw new ConflictException('비밀번호가 일치하지 않습니다.');
+    const tokens = await this.getTokens(existUser.id, existUser.email);
+    return tokens;
+  }
+
+  /**
+   * access token, refresh token 발급
+   * @author 김승일
+   * @param userId
+   * @param email
+   */
+  async getTokens(userId: number, email: string) {
+    const jwtPayload: JwtPayload = {
+      sub: userId,
+      email,
+    };
+
+    const [AccessToken, RefreshToken] = await Promise.all([
+      this.jwtService.signAsync(jwtPayload, {
+        secret: process.env.ACCESS_TOKEN_SECRET,
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+      }),
+      this.jwtService.signAsync(jwtPayload, {
+        secret: process.env.REFRESH_TOKEN_SECRET,
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+      }),
+    ]);
+    // RefreshToken 암호화 후 캐시에 저장
+    // const hashRefreshToken = bcrypt.hashSync(RefreshToken, 10);
+    // await this.cacheManager.set(`${userId}-refresh`, hashRefreshToken, {
+    //   ttl: 60 * 60 * 24 * 7, // 7일
+    // });
+    return { AccessToken, RefreshToken };
   }
 }
