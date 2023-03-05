430933ee106de9fa2571ef9d4aea98ac849f3195
kingseungil <109774037+kingseungil@users.noreply.github.com>
2023-03-05 13:37:53 +0900

    Feat : - gym 업데이트 시 변경하지 않는 컬럼 원래 값 유지(3항연산자) - swagger에 파일업로드 가능하도록 수정 #24

--- src/domain/gym/gym.decorators.ts
+++ src/domain/gym/gym.decorators.ts
@@ -1,10 +1,12 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiBearerAuth, ApiConsumes } from '@nestjs/swagger';
 
 export const GymSignup = () => {
   return applyDecorators(
     ApiOperation({ summary: '체육관 등록' }),
     ApiResponse({ status: 201, description: '체육관이 등록되었습니다.' }),
+    ApiBearerAuth('access-token'),
+    ApiConsumes('multipart/form-data'),
     ApiResponse({ status: 400, description: '등록 실패' })
   );
 };
@@ -12,6 +14,7 @@ export const GymSignup = () => {
 export const MyGymGet = () => {
   return applyDecorators(
     ApiOperation({ summary: '내 체육관 조회' }),
+    ApiBearerAuth('access-token'),
     ApiResponse({ status: 200, description: '조회 성공' }),
     ApiResponse({ status: 400, description: '조회 실패' })
   );
@@ -20,6 +23,8 @@ export const MyGymGet = () => {
 export const GymUpdate = () => {
   return applyDecorators(
     ApiOperation({ summary: '체육관 정보 수정' }),
+    ApiBearerAuth('access-token'),
+    ApiConsumes('multipart/form-data'),
     ApiResponse({ status: 200, description: '수정 성공' }),
     ApiResponse({ status: 400, description: '수정 실패' })
   );
@@ -28,16 +33,9 @@ export const GymUpdate = () => {
 export const GymDelete = () => {
   return applyDecorators(
     ApiOperation({ summary: '체육관 삭제' }),
+    ApiBearerAuth('access-token'),
     ApiResponse({ status: 200, description: '삭제 성공' }),
     ApiResponse({ status: 400, description: '삭제 실패' })
-  );
-};
-
-export const FileUpload = () => {
-  return applyDecorators(
-    ApiOperation({ summary: '파일 업로드' }),
-    ApiResponse({ status: 200, description: '업로드 성공' }),
-    ApiResponse({ status: 400, description: '업로드 실패' })
   );
 };
 
