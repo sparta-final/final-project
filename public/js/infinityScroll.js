da70edf7e35c8245b4c8ff6deef7e48399d93274
ghwns40@naver.com <ghwns40@naver.com>
2023-03-08 18:07:31 +0900

    feat: 무한스크롤 5개씩 조회하기 완료

--- public/js/infinityScroll.js
+++ public/js/infinityScroll.js
@@ -1,5 +1,4 @@
 const cards = document.querySelectorAll('.card');
-
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
@@ -13,44 +12,75 @@ const observer = new IntersectionObserver(
   }
 );
 
-const lastCardObserver = new IntersectionObserver((entries) => {
-  const lastCard = entries[0];
-  if (!lastCard.isIntersecting) return;
-  getGym();
-  lastCardObserver.unobserve(lastCard.target);
-  lastCardObserver.observe(document.querySelector('.card:last-child'));
-}, {});
-
-lastCardObserver.observe(document.querySelector('.card:last-child'));
-
-cards.forEach((card) => {
-  observer.observe(card);
-});
+const lastCardObserver = new IntersectionObserver(
+  (entries) => {
+    const lastCard = entries[0];
+    if (!lastCard.isIntersecting) return;
+    getGym();
+    lastCardObserver.unobserve(lastCard.target);
+  },
+  {
+    threshold: 0,
+  }
+);
 
 const cardContainer = document.querySelector('.card-container');
+let postCount = 0;
+let loading = false;
+const limit = 5;
+let rows = [];
+
+function loadNewCards(name) {
+  const card = document.createElement('div');
+  card.textContent = `${name}`;
+  card.classList.add('card');
+  cardContainer.append(card);
+  observer.observe(card);
+}
 
 function getGym() {
+  if (loading) return;
+  loading = true;
   axios
-    .get('api/gym')
+    .get('api/gym', {
+      params: {
+        offset: postCount,
+        limit,
+      },
+    })
     .then((res) => {
-      let rows = res.data;
-      for (let i = 0; i < rows.length; i++) {
-        let name = rows[i]['name'];
-        loadNewCrds(name);
-      }
-      function loadNewCrds(name) {
-        for (let i = 0; i < 1; i++) {
-          const card = document.createElement('div');
-          card.textContent = `${name}`;
-          card.classList.add('card');
-          observer.observe(card);
-          cardContainer.append(card);
+      rows = res.data; // rows를 업데이트
+      if (postCount === 0) {
+        cardContainer.innerHTML = '';
+        for (let i = 0; i < limit && i < rows.length; i++) {
+          const name = rows[i]['name'];
+          loadNewCards(name);
         }
+        postCount += limit;
+      } else {
+        const remainingPosts = rows.slice(postCount);
+        const maxPostsToLoad = Math.min(limit, remainingPosts.length);
+        for (let i = 0; i < maxPostsToLoad; i++) {
+          const name = remainingPosts[i]['name'];
+          loadNewCards(name);
+        }
+        postCount += maxPostsToLoad;
       }
+      if (postCount >= rows.length) {
+        lastCardObserver.observe(document.querySelector('.card-container:last-child .card:last-child'));
+      }
+      loading = false;
     })
     .catch((err) => {
       console.log(err);
     });
-
-  // observer.unobserve(document.getElementById('elementToObserve'));
 }
+
+getGym();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5 && !loading && postCount > 0 && postCount < rows.length) {
+    getGym();
+  }
+});
