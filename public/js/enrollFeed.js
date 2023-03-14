da6bcfe97cfe3c17641c6f0add6988a6c07f4bbf
HxxJ-project <hxxjxxxhxxx@gmail.com>
2023-03-14 20:45:56 +0900

    Fix : 헬스장 이미지 여러개 등록 수정

--- public/js/enrollFeed.js
+++ public/js/enrollFeed.js
@@ -2,15 +2,12 @@ function enrollFeed() {
   const content = document.getElementsByClassName('feed-textarea')[0].value;
   console.log('✨✨✨', content, '✨✨✨');
   const feedImg = document.getElementById('feedImg').files;
-
+  console.log('✨✨✨', feedImg, '✨✨✨');
   console.log(feedImg.files);
   const formData = new FormData();
   formData.append('content', content);
   for (let i in feedImg) {
     formData.append('feedImg', feedImg[i]);
-  }
-  for (let value of formData.values()) {
-    console.log(value);
   }
   axios
     .post('/api/feed', formData, {
